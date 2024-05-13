a9c57894f3f325c2b3a19149beff3281d9d3eddd
Thiago-Nascimento <thiago.r.nascimento@hotmail.com>
2024-05-13 15:27:52 -0300

    feat: Exemplos de função e eventos

--- exemploDOM.js
+++ exemploDOM.js
@@ -1,4 +1,4 @@
-document.body.innerHTML = "<h1>Teste H1</h1>"
+document.body.innerHTML += "<h1>Teste H1</h1>"
 
 document.body.innerHTML += '<p id="texto">Outro teste</p>'
 
@@ -6,12 +6,44 @@ let paragrafo = document.getElementById("texto")
 
 paragrafo.style.color = "blue"
 
-let opcao = window.prompt("Escolha a cor do paragrafo:\n1 - Verde\n2 - Vermelho")
+// let opcao = window.prompt("Escolha a cor do paragrafo:\n1 - Verde\n2 - Vermelho")
 
-if (opcao == "1") {
-    paragrafo.style.color = "darkgreen"
-} else if (opcao == "2") {
-    paragrafo.style.color = "red"
-} else {
-    window.alert("Opcao invalida")
+// if (opcao == "1") {
+//     paragrafo.style.color = "darkgreen"
+// } else if (opcao == "2") {
+//     paragrafo.style.color = "red"
+// } else {
+//     window.alert("Opcao invalida")
+// }
+
+let cliques = 0;
+
+const somarCliques = () => {
+    // cliques = cliques + 1
+    // cliques += 1
+    cliques++
+
+    // let spanCliques = document.querySelector("#num-cliques")
+    let spanCliques = document.getElementById("num-cliques")
+
+    spanCliques.innerHTML = cliques
 }
+
+let tituloNome = document.querySelector("#titulo-nome")
+
+const mudarNome = () => {
+    tituloNome.innerText = "Thiago Nascimento"
+}
+
+tituloNome.addEventListener("click", mudarNome)
+
+// document.addEventListener("DOMContentLoaded", () => alert("ALOUUUUUUUUUUU"))
+
+function voltarPagina() {
+    window.history.back()
+}
+
+function fecharPagina() {
+    window.close()
+}
+
